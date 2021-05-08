1fa44ba2adf389375f3d677c8427eecbd8c7e1b8
0aoq <a0oq@outlook.com>
2021-05-08 17:46:42 -0400

    Removed all data for pre-made threads.

--- data.js
+++ data.js
@@ -1,46 +1,4 @@
-var defaultThreads = [{
-        id: 0,
-        title: "Thread 1",
-        author: "Aaron",
-        date: new Date().toLocaleString(),
-        content: "Thread content",
-        locked: false,
-        comments: [{
-                number: 0,
-                author: "Jack",
-                date: new Date().toLocaleString(),
-                content: "Hey there"
-            },
-            {
-                number: 1,
-                author: "Arthur",
-                date: new Date().toLocaleString(),
-                content: "Hey to you too"
-            }
-        ]
-    },
-    {
-        id: 1,
-        title: "Thread 2",
-        author: "Aaron",
-        date: new Date().toLocaleString(),
-        content: "Thread content 2",
-        locked: false,
-        comments: [{
-                number: 0,
-                author: "Jack",
-                date: new Date().toLocaleString(),
-                content: "This is the second thread!"
-            },
-            {
-                number: 1,
-                author: "Arthur",
-                date: new Date().toLocaleString(),
-                content: "Yeah man!!"
-            }
-        ]
-    }
-]
+var defaultThreads = []
 
 var threads = defaultThreads
 
