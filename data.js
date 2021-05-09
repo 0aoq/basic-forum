03770f2e09789cdf6a4bb607a1111932dc889347
0aoq <a0oq@outlook.com>
2021-05-08 23:01:48 -0400

    (Minor) Updated default threads to show all thread types.

--- data.js
+++ data.js
@@ -1,4 +1,43 @@
-var defaultThreads = []
+var defaultThreads = [{
+        "id": 0,
+        "title": "/dev/",
+        "author": "You",
+        "date": "5/8/2021, 10:58:48 PM",
+        "content": "Development",
+        "locked": false,
+        "comments_allowed": false,
+        "comments": []
+    },
+    {
+        "id": 1,
+        "title": "Comments Enabled",
+        "author": "You",
+        "date": "5/8/2021, 10:59:03 PM",
+        "content": "",
+        "locked": false,
+        "comments_allowed": true,
+        "comments": [{
+                "number": 0,
+                "author": "You",
+                "date": "5/8/2021, 11:00:06 PM",
+                "content": "<p>Comment testing.</p>\n<h1 id=\"h1\">h1</h1>\n<h2 id=\"h2\">h2</h2>\n<h3 id=\"h3\">h3</h3>\n<h3 id=\"h4\">h4</h3>\n<h4 id=\"h5\">h5</h4>\n<h5 id=\"h6\">h6</h5>\n<pre><code>code block\n</code></pre>\n<blockquote>\n<p>blockquote</p>\n</blockquote>\n"
+            },
+            {
+                "number": "1",
+                "author": "[removed]",
+                "date": "[removed]",
+                "content": "[removed]"
+            }
+        ]
+    },
+    {
+        "id": "_",
+        "title": "[removed]",
+        "content": "[removed]",
+        "date": "(Removed Date) 5/8/2021, 11:00:26 PM",
+        "locked": true
+    }
+]
 
 var defaultUsers = [{
     id: 0,
