83514a4c1eca353c594cafaef8e72f66e8f1fa56
0aoq <a0oq@outlook.com>
2021-05-08 20:47:44 -0400

    (Major) Began work on the user JSON/profile page

--- data.js
+++ data.js
@@ -1,10 +1,25 @@
 var defaultThreads = []
 
+var defaultUsers = [{
+    id: 0,
+    username: "0aoq",
+    bio: "Default user profile.",
+    join_date: new Date().toLocaleString()
+}]
+
 var threads = defaultThreads
+var users = defaultUsers
 
 if (localStorage && localStorage.getItem('threads')) {
     threads = JSON.parse(localStorage.getItem('threads'));
 } else {
     threads = defaultThreads;
     localStorage.setItem('threads', JSON.stringify(defaultThreads));
+}
+
+if (localStorage && localStorage.getItem('users')) {
+    users = JSON.parse(localStorage.getItem('users'));
+} else {
+    users = defaultUsers;
+    localStorage.setItem('users', JSON.stringify(defaultUsers));
 }
